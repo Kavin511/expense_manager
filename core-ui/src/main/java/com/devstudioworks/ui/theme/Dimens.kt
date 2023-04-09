ccaba9a2c53a44c4f1f8d57eabc575c32c235d63
kavin511 <kavinalmighty@gmail.com>
2023-04-09 13:22:40 +0530

    Increased card corner radius

--- core-ui/src/main/java/com/devstudioworks/ui/theme/Dimens.kt
+++ core-ui/src/main/java/com/devstudioworks/ui/theme/Dimens.kt
@@ -2,5 +2,5 @@ package com.devstudioworks.ui.theme
 
 import androidx.compose.ui.unit.dp
 
-val DEFAULT_CARD_CORNER_RADIUS = 4.dp
+val DEFAULT_CARD_CORNER_RADIUS = 6.dp
 val DEFAULT_CARD_ELEVATION = 4.dp
