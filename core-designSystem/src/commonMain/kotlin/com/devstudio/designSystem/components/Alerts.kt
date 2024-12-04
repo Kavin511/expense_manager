6c92c1b292840f12294a1ecd768e0323a44e0414
Kavin511 <kavinalmighty@gmail.com>
2024-12-04 14:11:35 +0530

    fixed ui issue due to padding in bottom sheet

--- core-designSystem/src/commonMain/kotlin/com/devstudio/designSystem/components/Alerts.kt
+++ core-designSystem/src/commonMain/kotlin/com/devstudio/designSystem/components/Alerts.kt
@@ -1,26 +1,14 @@
 package com.devstudio.designSystem.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.ModalBottomSheetDefaults
-import androidx.compose.material3.ModalBottomSheetProperties
-import androidx.compose.material3.SheetState
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.devstudio.designSystem.appColors
 
 
 @Composable
@@ -67,27 +55,15 @@ fun BottomSheet(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
     sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
-    properties: ModalBottomSheetProperties = ModalBottomSheetDefaults.properties(),
+    properties: ModalBottomSheetProperties = ModalBottomSheetDefaults.properties,
     content: @Composable ColumnScope.() -> Unit,
 ) {
     ModalBottomSheet(
         onDismissRequest = { onDismissRequest.invoke() },
+        modifier = modifier,
         properties = properties,
         sheetState = sheetState,
     ) {
-        Column(modifier = Modifier.padding(bottom = getContentBottomPadding())) {
-            content()
-        }
-    }
-}
-
-@Composable
-private fun getContentBottomPadding(): Dp {
-    val bottomPadding =
-        WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding().value.toDouble()
-    return if (bottomPadding == 0.0) {
-        32.dp
-    } else {
-        WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+        content()
     }
 }
